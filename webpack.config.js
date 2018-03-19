095642e4534f33feac754a45520d341ea7848a49
Mark Talbot <mark.talbot@weebly.com>
2018-03-19 16:52:04 -0400

    Adding vue extensions to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': path.resolve(__dirname, 'src')
-    }
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   },
   devServer: {
     historyApiFallback: true,
